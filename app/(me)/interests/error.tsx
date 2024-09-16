ebccae0a7b784ff4ac8c8240c8916cd8e3a149af
Dayne Tran <daynetran256@gmail.com>
2024-09-15 22:32:17 -0700

    fix/errors-in-pages

--- app/(me)/interests/error.tsx
+++ app/(me)/interests/error.tsx
@@ -1,7 +1,11 @@
 "use client"
 
-import { Button } from "@/components/ui/button"
-import Link from "next/link"
+import Link from 'next/link'
+import Image from "next/image";
+
+import { Button } from '@/components/ui/button'
+import Penguino from "@/app/Penguinos.png";
+import { PostHeader } from '@/components/Headers/PostHeader';
 
 const Error = ({
     error,
@@ -11,15 +15,40 @@ const Error = ({
     reset: () => void
 }) => {
     return (
-        <div className="flex flex-col gap-4 justify-center items-center w-full h-screen " >
-            <h2>Something went wrong! The page may not exist (yet).</h2>
-            <Button asChild>
-                <Link href="/" >
-                    Return home.
-                </Link>
-            </Button>
+        <div className="flex flex-col justify-start items-center w-full h-screen">
+            <section className="flex flex-col items-center w-full overflow-y-auto overflow-x-clip pb-5 " >
+                <PostHeader
+                    frontmatter={{ title: "", }}
+                    showMetadata={false}
+                    readingTime={""}
+                />
+            </section>
+            <section className="flex flex-col gap-3 justify-center items-center w-full h-screen px-5">
+                <h3 className="font-bold text-base lg:text-2xl text-transparent bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text">
+                    404
+                </h3>
+                <h2 className="font-bold text-2xl py-2 lg:text-5xl text-transparent bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text ">
+                    Page cannot be found
+                </h2>
+                <div className="-my-12">
+                    <Image
+                        src={Penguino}
+                        alt="Penguino"
+                        width={500}
+                        height={500}
+                    />
+                </div>
+                <p className="text-center text-base">
+                    Sorry, these are not the penguins you are looking for. <br /> Try starting again from the homepage.
+                </p>
+                <Button size="sm" variant="sidebar" asChild>
+                    <Link href="/" className="text-base">Return Home</Link>
+                </Button>
+            </section>
         </div>
     )
 }
 
 export default Error;
+
+
